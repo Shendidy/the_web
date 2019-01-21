ac047b16c45c43be9d29c2c9c5c72e9c568e0f80
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-21 16:45:54 +0000

    Working with params.

--- app.rb
+++ app.rb
@@ -1,23 +1,13 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/' do
-  "Hello World!"
-end
-
-get '/test1' do
-  "Testing page test1"
-end
-
-get '/test2' do
-  "Testing page test2"
-end
-
-get '/test3' do
-  "Testing page test3"
-end
-
-get '/cat' do
+get '/random-cat' do
   @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
